5366b2460ac4cd7d9ed1722ad89207b91fca3059
Luis Ghidelli <luisfernando.ghidelli@gmail.com>
2025-06-24 12:02:11 +0100

    mover o game para o show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,11 +2,21 @@ class GamesController < ApplicationController
   def index
   end
 
+  def show
+    @game = Game.find(params[:id])
+
+    @current_question_index = 0
+
+    @question = Question.all
+  end
+
   def new
     @game = Game.new()
   end
 
   def create
     @game = Game.create!()
+
+    redirect_to game_path(@game)
   end
 end
